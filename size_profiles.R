6d16e5ea19db6ce0a1104525474edf9513e30a7b
groverj3 <groverj3@gmail.com>
2016-12-06 13:22:16 -0700

    Re-add size_profiles.R

---
+++ size_profiles.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+# R script to create small RNA size distribution line plots from input files with two columns (counts), (length)
+# Run on BASH command line, use 'find . -name "(p)" -exec "./size_profiles.R" {} \;' to batch process
+
+library(ggplot2)
+
+args = commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+
+readlengths <- read.table(input_file)
+
+ggplot(data = readlengths, aes(x = V2, y = V1)) +
+  geom_line(size = 2, color = "red") +
+  theme(plot.title = element_text(size = 30)) + # change axis titles
+  ylab("Reads") + # \n adds a new line, increases distance between label and axis
+  xlab("Length (nt)") +
+  scale_x_continuous(breaks = c(19:27),labels = c("19", "20", "21", "22", "23", "24", "25", "26", "27")) +
+  theme(axis.text = element_text(size = 22)) + # changes axis labels
+  theme(axis.title = element_text(size = 25)) + # change axis titles
+  theme(axis.title.y = element_text(margin = margin(0,15,0,0))) + # changes label distance to axis 
+  theme(axis.title.x = element_text(margin = margin(15,0,0,0)))
+
+ggsave(filename = paste(sub("\\.txt$", "", input_file), ".png", sep = ""), width = 7.5, height = 5) # saves output to .png based on input filename
+
+if(file.exists("Rplots.pdf")) file.remove("Rplots.pdf") # Removes unneeded Rplots.pdf that I can't suppress for some reason
