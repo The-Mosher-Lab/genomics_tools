3e5c3b56c86414ccc0d4d27c77ef089b9f5eeb89
groverj3 <groverj3@gmail.com>
2016-12-06 13:12:30 -0700

    Suppress generation of Rplots.pdf

--- size_profiles.R
+++ size_profiles.R
@@ -11,14 +11,18 @@ input_file <- args[1]
 readlengths <- read.table(input_file)
 
 ggplot(data = readlengths, aes(x = V2, y = V1)) +
-geom_line(size = 2, color = "red") +
-theme(plot.title = element_text(size = 30)) + # change axis titles
-ylab ("Reads") + # \n adds a new line, increases distance between label and axis
-xlab ("Length (nt)") +
-scale_x_continuous(breaks = c(19:27),labels = c("19", "20", "21", "22", "23", "24", "25", "26", "27")) +
-theme(axis.text = element_text(size = 22)) + # changes axis labels
-theme(axis.title = element_text(size = 25)) + # change axis titles
-theme(axis.title.y = element_text(margin = margin(0,15,0,0))) + # changes label distance to axis
-theme(axis.title.x = element_text(margin = margin(15,0,0,0)))
+  geom_line(size = 2, color = "red") +
+  theme(plot.title = element_text(size = 30)) + # Change axis titles
+  ylab("Reads") + # \n adds a new line, increases distance between label and axis if desired
+  xlab("Length (nt)") +
+  scale_x_continuous(breaks = c(19:27),labels = c("19", "20", "21", "22", "23", "24", "25", "26", "27")) +
+  theme(axis.text = element_text(size = 22)) + # Changes axis labels
+  theme(axis.title = element_text(size = 25)) + # Change axis titles
+  theme(axis.title.y = element_text(margin = margin(0,15,0,0))) + # Changes label distance to axis 
+  theme(axis.title.x = element_text(margin = margin(15,0,0,0)))
 
-ggsave(filename = paste(input_file, ".png", sep = ""))
+# Saves output to .png based on input filename without .txt extension, w and h may be changed as desired
+
+ggsave(filename = paste(sub("\\.txt$", "", input_file), ".png", sep = ""), width = 7.5, height = 5) 
+
+if(file.exists("Rplots.pdf")) file.remove("Rplots.pdf") # Removes unneeded Rplots.pdf that I can't suppress for some reason
