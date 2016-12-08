a6dfde90af60fe3288c00dd5747b15a99ca02d67
groverj3 <groverj3@gmail.com>
2016-12-08 16:22:13 -0700

    Better comments

--- fastq_length_profile.sh
+++ fastq_length_profile.sh
@@ -4,4 +4,6 @@
 # Created: 12/2016
 # Purpose: Reads fastq files and outputs counts of read lengths within the file
 
+# Use 'find . -name "(*.fastq)" -exec "./fastq_length_profile.sh" {} \;' to batch process
+
 cat "$1" | awk '{if(NR%4==2) print length($1)}' | sort | uniq -c > "${1%.fastq}.readlengths.txt"
