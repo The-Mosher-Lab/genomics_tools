b50535ab468a8178c72f8302593f4a55aa279664
groverj3 <groverj3@gmail.com>
2016-12-08 12:48:10 -0700

    Updated comments

--- fasta_count_contigs.sh
+++ fasta_count_contigs.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Author: Jeffrey Grover
 # Created: 11/2016
 # Purpose: Count number of chromosomes/contigs in a genome fasta
+
+# Takes one command-line option as input file, outputs to stdout
 
 fgrep -o ">" "$1" | wc -l
