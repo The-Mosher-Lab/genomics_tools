a48cab8fd58aeb137de4dbf1d6bbf3565c6d6947
groverj3 <groverj3@gmail.com>
2017-07-11 16:24:27 -0700

    Combine the shortstack output files; Results.txt and Counts.txt

--- shortstack_results_counts_combine.sh
+++ shortstack_results_counts_combine.sh
@@ -5,7 +5,6 @@
 # Purpose: Combine the shortstack output files; Results.txt and Counts.txt
 
 # Accepts two filenames as input. File 1 will be Results.txt, File 2 is Counts.txt
-# Outputs to a new file called "shortstack_full_report.txt"
+# Outputs to a new file called "shortstack_full_report.csv"
 
-join --header "$1" "$2" | cut -d ' ' --complement -f22,23 | tr ' ' ',' > shortstack_full_report.txt
-
+join --header "$1" "$2" | cut -d ' ' --complement -f22,23 | tr ' ' ',' > shortstack_full_report.csv
