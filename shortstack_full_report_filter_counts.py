a6808ce0e35a4b81dc8b36d8e0cd0c4289ec3bb0
groverj3 <groverj3@gmail.com>
2017-07-25 09:56:42 -0700

    Filter the combined "shortstack_full_report.csv" for loci expressed above a threshold in one sample.

---
+++ shortstack_full_report_filter_counts.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Author: Jeffrey Grover
+# Purpose: Filter the combined "shortstack_full_report.csv" for loci expressed above a threshold in one sample.
+# Created: 7/2017
+
+import csv
+from argparse import ArgumentParser
+
+# Define function to filter output loci for expression above a threshold in one sample
+
+
+def filter_by_expression(input_report, threshold, column, output_results):
+    with open(input_report, 'r') as input_handle:
+        input_csv = csv.reader(input_handle)
+        with open(output_results, 'w') as output_handle:
+            output_csv = csv.writer(output_handle)
+            output_csv.writerow(next(input_csv))
+            for row in input_csv:
+                sample = int(row[column])
+                if sample >= threshold:
+                    output_csv.writerow(row)
+
+# Parse command line options
+
+parser = ArgumentParser(description='Filter the combined "shortstack_full_report.csv" for loci with expression at or '
+                                    'above a threshold in one sample.')
+parser.add_argument('-t', '--threshold', help='Expression threshold to test.', type=int)
+parser.add_argument('-c', '--column', help='The column in "shortstack_full_report.csv" for the sample to test.',
+                    type=int)
+parser.add_argument('input_path', help='File to process', metavar='File')
+
+library = parser.parse_args().column - 1  # Because humans count starting at 1 not 0
+expression = parser.parse_args().threshold
+input_path = parser.parse_args().input_path
+output_path = 'shortstack_full_report_col%s_exp%s.csv' % (library + 1, expression)
+
+# Process the file
+
+filter_by_expression(input_path, expression, library, output_path)
