8b3b88ee871b95fcaf579f0072d8dfd8271bbfca
groverj3 <groverj3@gmail.com>
2017-10-15 22:37:13 -0700

    Reports sRNA loci that overlap with a gff3 feature of choice

---
+++ shortstack_full_report_gff3_overlap.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Author: Jeffrey Grover
+# Purpose: Find small RNA clusters from a ShortStack "full report" file that overlap with features in a gff3
+# Created: 10/2017
+
