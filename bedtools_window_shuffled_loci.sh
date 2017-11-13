8a2846ceec38e093fdeb300c85c6f9c972a7e79f
groverj3 <groverj3@gmail.com>
2017-11-13 11:25:09 -0700

    Use basename

--- bedtools_window_shuffled_loci.sh
+++ bedtools_window_shuffled_loci.sh
@@ -7,8 +7,9 @@
 
 # Pass through -l -r -a options in that order. Runs on all bed files in the same directory.
 
+filename=$(basename "$3")
 for bed_file in ./*.bed
 do
     bedtools window -c -l "$1" -r "$2" -a "$3" -b "$bed_file" | \
-    awk '{sum+=$7;} END { print sum }' >> "${3}_${1}_up_${2}_down_overlaps.txt"
+    awk '{sum+=$7;} END { print sum }' >> "${filename}_${1}_up_${2}_down_overlaps.txt"
 done
