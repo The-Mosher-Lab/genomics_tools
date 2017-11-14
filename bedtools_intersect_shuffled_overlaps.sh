391f220c52ed163128b367b88328b47b8f74aef4
groverj3 <groverj3@gmail.com>
2017-11-14 15:52:20 -0700

    Better comments

--- bedtools_intersect_shuffled_overlaps.sh
+++ bedtools_intersect_shuffled_overlaps.sh
@@ -5,8 +5,6 @@
 # Purpose: Run bedtools window on a large number of files with randomized loci
 # To get the counts of nearby and overlapped features
 
-# Pass through -l -r -a options in that order. Runs on all bed files in the same directory.
-
 filename=$(basename "$1")
 shuffled_dir="$2"
 for bed_file in ${shuffled_dir}*.bed
