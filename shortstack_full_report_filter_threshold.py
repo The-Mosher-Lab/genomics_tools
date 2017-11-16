5d3d923cd31cba4f3f534cf0d4fe3af23027a28a
groverj3 <groverj3@gmail.com>
2017-11-16 12:49:06 -0700

    Added commandline options

--- shortstack_full_report_filter_threshold.py
+++ shortstack_full_report_filter_threshold.py
@@ -11,17 +11,19 @@ from argparse import ArgumentParser
 # Define function to filter output loci for expression above a threshold
 
 
-def filter_by_expression(input_report, threshold, column, direction):
+def filter_by_expression(input_report, threshold, test_column, control_column,
+                         direction):
     with open(input_report, 'r') as input_handle:
         input_reader = csv.reader(input_handle)
         print(','.join(next(input_reader)))
         for row in input_reader:
-            locus_expression = float(row[column])
+            locus_expression = float(row[test_column])
+            control_expression = float(row[control_column])
             if direction == 'above':
-                if locus_expression >= threshold:
+                if locus_expression >= threshold * control_expression:
                     print(','.join(row))
             elif direction == 'below':
-                if locus_expression <= threshold:
+                if locus_expression <= threshold * control_expression:
                     print(','.join(row))
 
 
@@ -31,20 +33,22 @@ parser = ArgumentParser(
     description='Filter the combined "shortstack_full_report.csv" for loci '
     'with expression at or above a threshold in one sample.')
 parser.add_argument(
-    '-t',
     '--threshold',
     help='Expression threshold to test (as a decimal).',
     type=float,
     metavar='FLOAT')
 parser.add_argument(
-    '-d',
     '--direction',
     choices=('above', 'below'),
     help='Direction to test expression vs control.')
 parser.add_argument(
-    '-c',
-    '--column',
+    '--testcolumn',
     help='The column in "shortstack_full_report.csv" for the sample to test.',
+    type=int,
+    metavar='INT')
+parser.add_argument(
+    '--control',
+    help='The column in "shortstack_full_report.csv" for the control sample.',
     type=int,
     metavar='INT')
 parser.add_argument(
@@ -52,11 +56,13 @@ parser.add_argument(
     help='Shortstack full report (.csv) to process.',
     metavar='File')
 
-column = parser.parse_args().column - 1  # Because humans count starting at 1
+test_column = parser.parse_args().testcolumn - 1  # Humans count starting at 1
+control_column = parser.parse_args().control - 1
 threshold = parser.parse_args().threshold
 input_path = parser.parse_args().input_path
 direction = parser.parse_args().direction
 
 # Process the file
 
-filter_by_expression(input_path, threshold, column, direction)
+filter_by_expression(input_path, threshold, test_column, control_column,
+                     direction)
