d9947d68e5f7ec5abe151f4ca6a2a80b4cbc199f
groverj3 <groverj3@gmail.com>
2017-12-05 15:35:43 -0700

    Code cleanup

--- fastq_length_filter.py
+++ fastq_length_filter.py
@@ -4,36 +4,32 @@
 # Purpose: Filter .fastq reads between sizes defined by user input.
 # Created: 12/2016
 
-# The first option is an input fastq file, --min and --max are min and max length to filter
-# The output file will be another fastq with the length filtering appended to the filename
-# Use find -exec to batch process: ex. 'find . -name "*.fastq" -exec "./fastq_length_filter.py" {} --min 25 --max 27 \;'
+# Use find -exec to batch process:
+# find . -name "*.fastq" -exec "./fastq_length_filter.py" {} --min 19 --max 26 \;
 
-import re
 from argparse import ArgumentParser
-
-
-# Define function to iterate over fastq reads and output reads to new file if between min and max length
 
 
 def filter_by_length(input_path, output_path, min_length, max_length):
     n = 0
     seq_list = []
-    with open(input_path, 'r') as input_file:
-        with open(output_path, 'w') as output_file:
-            for line in input_file:
-                n += 1
-                seq_list.append(line)
-                if n == 4:
-                    if min_length < len(seq_list[1]) <= (max_length + 1):  # Had to do it this way to get it to work
-                        [output_file.write('%s' % item) for item in seq_list]
-                    n = 0
-                    seq_list = []
+    with open(input_path, 'r') as input_file, \
+         open(output_path, 'w') as output_file:
+        for line in input_file:
+            n += 1
+            seq_list.append(line)
+            if n == 4:
+                if min_length < len(seq_list[1]) <= (max_length + 1):
+                    [output_file.write('%s' % item) for item in seq_list]
+                n = 0
+                seq_list = []
 
 
 # Parse command line options
 
 parser = ArgumentParser(
-    description='Filters a given fastq file for reads between a supplied minimum and maximum length')
+    description='Filters a given fastq file for reads between a supplied'
+    'minimum and maximum length')
 parser.add_argument('input_path', help='Input .fastq file', metavar='File')
 parser.add_argument('--min', help='Minimum length for filtering', type=int)
 parser.add_argument('--max', help='Maximum length for filtering', type=int)
@@ -41,7 +37,8 @@ parser.add_argument('--max', help='Maximum length for filtering', type=int)
 input_fastq = parser.parse_args().input_path
 min_fastq = parser.parse_args().min
 max_fastq = parser.parse_args().max
-output_fastq = re.sub(r'\.fastq$', '.filtered_%s-%s.fastq' % (min_fastq, max_fastq), input_fastq)
+output_fastq = input_fastq.rstrip('.fastq') + '.filtered_%s-%s.fastq' % (
+    min_fastq, max_fastq)
 
 # Filter the .fastq
 
