d9a70ec828d06b668a1291445a61eb4d839d909a
groverj3 <groverj3@gmail.com>
2018-05-22 12:04:27 -0700

    Actually works now on test data

--- fastq_subsample_reads.py
+++ fastq_subsample_reads.py
@@ -9,16 +9,25 @@
 
 import random
 import subprocess
+import gzip
 from sys import exit
 from argparse import ArgumentParser
 
 # Functions
 
 
-def count_fastq_records(fastq_file):
-    fastq_lines = int(subprocess.getoutput('wc -l' + fastq_file))  # wc is fast
+def count_fastq_records(fastq_file, gzipped):  # wc -l is faster than python3
+    if gzipped:
+        fastq_lines = int(
+            subprocess.getoutput('zcat ' + fastq_file + ' | wc -l').split()[0])
+    else:
+        fastq_lines = int(
+            subprocess.getoutput('wc -l ' + fastq_file).split()[0])
     num_fastq_records = int(fastq_lines / 4)
-    return num_fastq_records
+    if fastq_lines % 4 == 0:
+        return num_fastq_records
+    else:
+        exit('Invalid .fastq file. Lines not divisible by 4.')
 
 
 def get_random_indices(num_fastq_records, reads_to_sample):
@@ -28,19 +37,35 @@ def get_random_indices(num_fastq_records, reads_to_sample):
     return random_indices
 
 
-def subsample(fastq_file, num_fastq_records, random_indices, output_file):
+def subsample(fastq_file, num_fastq_records, random_indices, output_file,
+              gzipped):
     record_number = 0
-    with open(fastq_file, 'r') as input_handle, open(output_file,
-                                                     'w') as output_handle:
-        for read_id in input_handle:
-            record_number += 1
-            if record_number in random_indices:
-                output_handle.write(read_id)
-                for i in range(3):
-                    output_handle.write(next(input_handle))  # Output record
-            else:
-                for i in range(3):
-                    next(input_handle)  # Skip record
+    if gzipped:
+        with gzip.open(fastq_file, 'rb') as input_handle, open(
+                output_file, 'wb') as output_handle:
+            for read_id in input_handle:
+                record_number += 1
+                if record_number in random_indices:
+                    output_handle.write(read_id)
+                    for i in range(3):
+                        output_handle.write(
+                            next(input_handle))  # Output record
+                else:
+                    for i in range(3):
+                        next(input_handle)  # Skip record
+    else:
+        with open(fastq_file, 'r') as input_handle, open(output_file,
+                                                         'w') as output_handle:
+            for read_id in input_handle:
+                record_number += 1
+                if record_number in random_indices:
+                    output_handle.write(read_id)
+                    for i in range(3):
+                        output_handle.write(
+                            next(input_handle))  # Output record
+                else:
+                    for i in range(3):
+                        next(input_handle)  # Skip record
 
 
 # Parse command line options
@@ -53,20 +78,29 @@ parser.add_argument(
     '-f', '--fraction', type=float, help='Fraction of reads to sample')
 parser.add_argument(
     '-n', '--number', type=int, help='Number of reads to sample')
+parser.add_argument(
+    '-z',
+    '--gzipped',
+    action='store_true',
+    help='Indicate whether your .fastq file is gzipped or not')
 
 fastq_file = parser.parse_args().input_file
 fraction = parser.parse_args().fraction
 number = parser.parse_args().number
+gzipped = parser.parse_args().gzipped
+
+# Validate parameters for subsampling
 
 if fraction and number:
     exit('Specify either a number of reads or a percentage, not both.')
-
 if not fraction and not number:
     exit('You must specify either a fraction or number of reads to sample.')
+if not fastq_file:
+    exit('You must specify an input .fastq file.')
 
 # Count fastq records
 
-num_fastq_records = count_fastq_records(fastq_file)
+num_fastq_records = count_fastq_records(fastq_file, gzipped)
 
 # Determine reads to sample
 
@@ -83,4 +117,4 @@ output_file = fastq_file.split('.fastq')[0] + (
 # Generate random indices and output
 
 random_indices = get_random_indices(num_fastq_records, reads_to_sample)
-subsample(fastq_file, num_fastq_records, random_indices, output_file)
+subsample(fastq_file, num_fastq_records, random_indices, output_file, gzipped)
