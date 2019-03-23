dd86e9e4d40620a2b7eda85b4aae934b3684a623
Jeffrey Grover <groverj3@gmail.com>
2019-03-22 22:41:03 -0700

    Unnecessary script

--- bed_grep.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Author: Jeffrey Grover
-# Created: 11/2017
-# Purpose: grep lines from a bed file to an output bed file (subset the bed)
-# Pass through the file to run as a positional variable
-
-filename=$(basename "$2")
-grep "$1" "$2" > "${filename%.bed}_${1}.bed"
