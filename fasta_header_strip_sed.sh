b1e2757566f2a86256db1862a2bce038160e3caf
Jeffrey Grover <groverj3@gmail.com>
2019-03-27 09:34:42 -0700

    Superseded by a python script I wrote.

--- fasta_header_strip_sed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Author: Jeffrey Grover
-# Created: 12/2017
-# Purpose: Strips garbage from .fasta headers, edit the script as needed.
-
-sed 's/|.*$//' "$1" > "${1%.fasta}.fixed_ids.fasta"
