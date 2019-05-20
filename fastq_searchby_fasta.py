d67569c43d4992c02496371bc4f13be685a19e15
Jeffrey Grover <groverj3@gmail.com>
2019-05-20 14:39:23 -0700

    Added more scripts

--- fastq_searchby_fasta.py
+++ fastq_searchby_fasta.py
@@ -7,7 +7,6 @@
 # This was created to search through small RNA fastq files for exact matches to
 # a list of queries in .fasta format and output a list of counts.
 
-import re
 from argparse import ArgumentParser
 from collections import OrderedDict
 
@@ -26,24 +25,24 @@ def load_fasta(input_fasta):
 
 
 def search_fastq(input_fastq, search_dict):
-    n = 0
     with open(input_fastq, 'r') as input_handle:
+        n = 0
         for line in input_handle:
-            if n == 1 and line.strip() in search_dict:
+            n += 1
+            if n == 2 and line.strip() in search_dict:
                 search_dict[line.strip()]['count'] += 1
             elif n == 4:
                 n = 0
-            else:
-                n += 1
     return search_dict
 
 
 def output_counts(input_dict, lib_name):
     print('ID', 'sequence', lib_name, sep=',')
     for sequence in input_dict:
-        print(
-            input_dict[sequence]['id'], sequence, input_dict[sequence]['count'],
-            sep=',')
+        print(input_dict[sequence]['id'],
+              sequence,
+              input_dict[sequence]['count'],
+              sep=',')
 
 
 # Parse command line options
@@ -52,9 +51,16 @@ parser = ArgumentParser(
     description=
     'Reads a list of sequences in .fasta format and counts occurances of that '
     'sequence in a .fastq file.')
-parser.add_argument('--fa', help='Input .fasta', metavar='File')
-parser.add_argument('--fq', help='Input .fastq', metavar='File')
-parser.add_argument('-n', '--name', help='Library ID or name', metavar='String')
+parser.add_argument('--fa',
+                    help='Input .fasta',
+                    metavar='File')
+parser.add_argument('--fq',
+                    help='Input .fastq',
+                    metavar='File')
+parser.add_argument('-n',
+                    '--name',
+                    help='Library ID or name',
+                    metavar='String')
 
 input_fasta = parser.parse_args().fa
 input_fastq = parser.parse_args().fq
